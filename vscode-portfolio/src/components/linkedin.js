13a6f46bd45f812613c66006d6ac01d13863d996
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-06-19 16:01:20 -0500

    Added linkedin activity and styled

--- vscode-portfolio/src/components/linkedin.js
+++ vscode-portfolio/src/components/linkedin.js
@@ -12,9 +12,16 @@ function LinkedIn() {
         <img src={bannerImg} alt='LinkedIn banner' className='banner'></img>
         <img src={headshot} alt='Headshot' className='headshot'></img>
         <div className='banner-info'>
-          <strong>David Alsabrook</strong>
-          <img src={shield} alt='Linkedin shield' className='shield'></img>
-          <p>(He / Him)</p>
+          <div className='banner-name'>
+            <strong>David Alsabrook</strong>
+            <img src={shield} alt='Linkedin shield' className='shield'></img>
+            <p>(He / Him)</p>
+          </div>
+          <p>Full Stack Web Developer</p>
+        </div>
+        <div className='activity-list'>
+          <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7187156239913574400" height="544" width="80%" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
+          <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7187156239913574400" height="544" width="80%" frameborder="0" allowfullscreen="" title="Embedded post"></iframe>
         </div>
       </section>
     </main>
