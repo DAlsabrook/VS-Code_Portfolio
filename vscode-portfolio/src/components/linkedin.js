3ea7a8125bc23d82cbf367f2f016fe07ab338699
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-06-19 15:42:37 -0500

    Started working on linkedin page

--- vscode-portfolio/src/components/linkedin.js
+++ vscode-portfolio/src/components/linkedin.js
@@ -1,11 +1,22 @@
 import '../styles/linkedin.css';
 
+// Images
+import bannerImg from '../images/linkedin/LinkedIn-Banner.png';
+import headshot from '../images/linkedin/1-backyard_headshot.jpeg'
+import shield from '../images/linkedin/LinkedIn-Shield.svg';
+
 function LinkedIn() {
   return (
     <main className="LinkedIn">
-      <div className="content-container">
-        <p>LinkedIn page</p>
-      </div>
+      <section className="linkedin-card">
+        <img src={bannerImg} alt='LinkedIn banner' className='banner'></img>
+        <img src={headshot} alt='Headshot' className='headshot'></img>
+        <div className='banner-info'>
+          <strong>David Alsabrook</strong>
+          <img src={shield} alt='Linkedin shield' className='shield'></img>
+          <p>(He / Him)</p>
+        </div>
+      </section>
     </main>
   )
 }
