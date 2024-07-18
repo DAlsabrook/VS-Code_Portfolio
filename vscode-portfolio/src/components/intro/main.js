8fc4693139a6e93fa83af072f8474561bb3b55cb
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-07-18 01:29:42 -0500

    Messing around with parallax

--- vscode-portfolio/src/components/intro/main.js
+++ vscode-portfolio/src/components/intro/main.js
@@ -10,14 +10,19 @@ const url = (name, wrap = false) =>
 // Each specifically colored section talks about tech and goal for coresponding section
 // Change cloud to bits of code
 // Make images code related
+
+// Offset 0 = top, 1 = uxui, 2 = vscode, 3 = backend
+// Factor = vh
 function IntroTab() {
   const parallax = useRef(null);
   return (
     <div className="introTab">
-      <Parallax ref={parallax} pages={3}>
-        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
-        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+      <Parallax ref={parallax} pages={4}>
+        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: 'white' }} factor={1.3}/>
+        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: 'var(--color-background-dark)' }} factor={1.3}/>
+        <ParallaxLayer offset={3} speed={1} style={{ backgroundColor: 'black' }} />
 
+        {/* Stars background img */}
         <ParallaxLayer
           offset={0}
           speed={0}
@@ -28,10 +33,12 @@ function IntroTab() {
           }}
         />
 
+        {/* Satellit img */}
         <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
           <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
         </ParallaxLayer>
 
+        {/* All of the clouds */}
         <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
           <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
           <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
@@ -59,7 +66,7 @@ function IntroTab() {
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={2.5}
+          offset={3.5}
           speed={-0.4}
           style={{
             display: 'flex',
@@ -67,11 +74,13 @@ function IntroTab() {
             justifyContent: 'center',
             pointerEvents: 'none',
           }}>
+            {/* Earth */}
           <img src={url('earth')} style={{ width: '60%' }} />
         </ParallaxLayer>
 
+        {/* Left and right browsers at bottom */}
         <ParallaxLayer
-          offset={2}
+          offset={3}
           speed={-0.3}
           style={{
             backgroundSize: '80%',
@@ -89,6 +98,7 @@ function IntroTab() {
             alignItems: 'center',
             justifyContent: 'center',
           }}>
+            {/* First intro image */}
           <img src={url('server')} style={{ width: '20%' }} />
         </ParallaxLayer>
 
@@ -101,11 +111,12 @@ function IntroTab() {
             alignItems: 'center',
             justifyContent: 'center',
           }}>
+            {/* Black broswer */}
           <img src={url('bash')} style={{ width: '40%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer
-          offset={2}
+          offset={3}
           speed={-0}
           style={{
             display: 'flex',
@@ -113,6 +124,7 @@ function IntroTab() {
             justifyContent: 'center',
           }}
           onClick={() => parallax.current.scrollTo(0)}>
+            {/* Main big browser */}
           <img src={url('clients-main')} style={{ width: '40%' }} />
         </ParallaxLayer>
       </Parallax>
